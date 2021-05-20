c16dc0e090a9336bb9ce334538fab6bd6880fc36
Konstantin Tennhard <konstantin@tennhard.net>
2021-05-20 14:19:27 -0400

    Hash serialization plugin
    Support serialization through `#to_hash` and `#to_h` and deserialization
    through augmenting the constructor. The plugin can be used in the
    following two ways:
    
    1. `include SmartProperties(SmartProperties::Plugins::HashSerialization)`
    2. `include SmartProperties(SmartProperties::Plugins::HashSerialization.without_optionals)`
    
    In the latter case, `#to_hash` will remove all keys that point to a
    `nil` value during serialization.

--- lib/smart_properties/plugin.rb
+++ lib/smart_properties/plugin.rb
@@ -11,5 +11,14 @@ module SmartProperties
     def attach(target_class)
       target_class.send(type, self)
     end
+
+    def customize(&customizations)
+      original_implementation = self
+      self.class.new(type) do
+        include original_implementation
+        define_singleton_method(:included, &original_implementation.method(:included))
+        module_eval(&customizations)
+      end
+    end
   end
 end
